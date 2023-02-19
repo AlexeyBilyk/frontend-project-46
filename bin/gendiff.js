07263aa4f80979f17b383addd4dc953ac3917f32
Alexey Bilyk <maverick4730@gmail.com>
2023-02-19 13:11:31 +0300

    sa

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,10 +5,14 @@ const program = new Command();
 program
     .name('gendiff')
     .description('Compares two configuration files and shows a difference.')
-    .version('0.1')
+    .version('0.0.1')
     .help('display help for command')
+    .argument('<filepath1>', 'link to first file')
+    .argument('<filepath2>', 'link to second file')
     .option('-V, --version', 'output the version number')
-    .option('-h, --help', 'output commad list');
+    .option('-f, --format <type>', 'output format')
+    .option('-h, --help', `display ${help} for ${command}`, 'blue');
+
 
 
 program.parse();
